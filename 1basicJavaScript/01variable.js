129c532f84dc3f3630c2411d6724d37a2f35d702
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-05 07:27:40 +0000

    commit change done

---
+++ 1basicJavaScript/01variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let accountEmail = "shailendra@gmail.com";
+var accountPassword = "1234522"
+accountCity = "bhopal"
+let accountState ="MP"
+console.log(accountId);
+console.table([accountId, accountPassword, accountCity,accountState]);
+
+/*
+ prefer not to use var beacuse beacuse of issue in block of scope and functional scope
+
+ */
---
+++ 1basicJavaScript/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(33+3)  //we are using node js ,not beowser 
+
+
+console.log(3+3);
+console.log("shailedra")
+
+let name ="shailedra"
+let age =10;
+let accountId = true;
+
+// number => 2 to power 53
+// BigInt
+// String
+// Boolean =true/false 
+// null => standalone value
+// undefined
+// Symobl = unique
+
+
+//object 
+console.log(typeof   null);
+
