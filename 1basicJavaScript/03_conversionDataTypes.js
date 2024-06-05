4a055b8486c2e4d848507cde7863a4b5e3a58cfe
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-05 12:38:41 +0000

    Succesfully commited changed

---
+++ 1basicJavaScript/03_conversionDataTypes.js
@@ -0,0 +1,19 @@
+// let score ="33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber =Number(score);
+// console.log(typeof valueNumber);
+
+ // "33"=>33
+// "33abc" => NaN
+// true => 1;false=>0
+// let isLogedIn= 1;
+let isLogedIn ="";
+let booleanIsLogedIn=Boolean(isLogedIn);
+
+console.log(booleanIsLogedIn);
+
+
+
