e959dbacc9da6595bcac64bcc4c079f436481aa1
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-06 11:16:32 +0000

    commit chnage done

---
+++ 1basicJavaScript/05_DataTypesSummery.js
@@ -0,0 +1,44 @@
+// //  2 types od dataTypes 
+// // 1 . primitive  === call by value 
+// // 2. Non primitive === call by refrence 
+
+// Primitive data are of 7 types 
+
+// 7 =[String , Number ,Boolean ,null, undefined , Symbol ,BigInt];
+
+
+// Non primitive DataTypes
+
+// [Array , Objects , Functions]
+
+
+// const score =100
+// const scoreValue =100.3;
+
+// const isLoggedId =false;
+// const outsideTemp =null;
+// let userEmail;
+
+// const id = Symbol('123');
+
+// const anotherId = Symbol('123');
+
+
+// Non primitive
+//  const heros =["shaktiman" ,"superheroes", "doga"];
+    //   key value pairs 
+
+    
+//  let obj ={ 
+//     name:"shailendra verma ",
+//     age:22,
+// }
+
+// typeof  use to cheack the dataTypes
+//  function 
+
+//  const myunction =function (){
+//     console.log("hello world");
+//  }
+
+//  console.log(typeof null);
