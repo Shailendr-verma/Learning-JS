e6e47d775d54613237d9c263d79631f057a4510e
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-06 10:43:28 +0000

    commit change done

---
+++ 1basicJavaScript/04_conversion.js
@@ -0,0 +1,23 @@
+// console.log("2" >1);
+// console.log(2 >= 1);
+// console.log (2 < 1);
+// console.log(2 <=1);
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >= 0);
+
+
+// Always avoid this type of convrsion always confussion 
+
+// console.log(undefined == 0);
+
+// console.log(undefined > 0);
+
+// console.log(undefined < 0);
+
+
+//  Strickly cheack ===
+
+
+// console.log(2=== 2);
