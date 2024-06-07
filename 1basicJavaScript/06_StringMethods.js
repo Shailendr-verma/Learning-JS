9603543b7dcfe6208744fb0b27f4650a4ab1611a
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-07 12:39:26 +0000

    commit change done

---
+++ 1basicJavaScript/06_StringMethods.js
@@ -0,0 +1,19 @@
+// const name ="shailendra verma"
+
+// const surname=" patel";
+
+// // String InterPolation 
+// console.log(`the complete name is${name} ${surname}`);
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.substring(1,6));
+
+const gamename ="   shailendra"
+
+console.log(gamename.toUpperCase());
+console.log(gamename.slice(1,5));
+
+console.log(gamename.trim());
+console.log(gamename.replace("len","___"));
+console.log(gamename.includes("sunder"));
