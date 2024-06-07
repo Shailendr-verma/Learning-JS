b20c569476afcafdeaefded11fce7f59fb33f140
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-07 13:07:41 +0000

    change commit done

---
+++ 1basicJavaScript/07_Number_Math.js
@@ -0,0 +1,59 @@
+// in javaScript is dynamically typed laaguge but we also can deine it specific
+
+// const score =400;
+// console.log(score);;
+
+// // exect define its a Number
+// const balance = new Number(400);
+// console.log(balance);
+
+// // convert int string and also perform string property it that's
+
+// console.log(balance.toString().length);
+
+// // toFixed use to how many number you want after the decimal 
+
+// console.log(balance.toFixed(5));
+
+
+// const othernumber = 123.890;
+// to round of the number and give approx value only use when we knoe the number of digit 
+
+// console.log(othernumber.toPrecision(3));
+
+
+// use to write digit in indian style we can also write it in every country thorugh
+// const hundred  = 100000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ------------------------------*********************----------****************-----------*************************
+// Math System in javaScript
+
+// maths library default in Js 
+
+// console.log(Math);
+
+// Math.abs  are use to  absoulute value only change into negative to positive not into positive to negatic 
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.5)); //to round of the values 
+// console.log(Math.ceil(4.96));   // to cheack the  top values 
+// console.log(Math.floor(4.96));  // to cheack the bottom value
+
+// console.log(Math.sqrt(225));the the swuare root of value
+
+// console.log(Math.random());
+// Math.random always give the value between 0 or 1 and many decimal 
+
+// console.log(Math.random()*10);
+// to placed on digit value by mltiple of 10
+// console.log((Math.random()*10) +1);
+
+
+// Formula to give random aject value between min and max 
+
+// const min =10;
+// const max= 20;
+// console.log(Math.floor(Math.random()*(max -min +1)+min));
+
