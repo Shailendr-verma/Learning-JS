7a1c13b66a8a675cb7d6e498b8e197d50b351aea
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-08 07:22:38 +0000

    done

---
+++ 1basicJavaScript/08_Datein.js
@@ -0,0 +1,34 @@
+// console.log("hello");
+
+// let mydate =new Date()
+// console.log(mydate.toDateString());
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+
+// let mycreateDate= new Date(2023,0,14)
+
+// let cretedDate =new Date("01-14-2023");
+
+// console.log(mycreateDate.toLocaleString());
+
+
+// let mytimeStamp =Date.now()
+// console.log(mytimeStamp);
+
+// console.log(Math.floor(Date.now()/100));
+
+let newDate =new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday:"long",
+    timeZone:
+})
+
+
+
+
+
+
