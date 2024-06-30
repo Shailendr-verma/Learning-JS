c4493aff0d48d0ef1c2a07206b4c9654da897535
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-06-30 12:31:26 +0000

    commit change

--- 02ArraysinJS/04_Ojects.js
+++ 02ArraysinJS/04_Ojects.js
@@ -1,20 +1,20 @@
 // Single toon 
 // const tinderUser = new Object()
 // non single toon boject
-const tinderUser ={}
+const tinderUser = {}
 
 tinderUser.id = "123dshailendra "
-tinderUser.name ="shailendra"
-tinderUser.isLoggedIn =false
+tinderUser.name = "shailendra"
+tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
-const regularUser ={
-    email :"shailen@gmail.com",
-    fullname:{
-        userfullname :{
+const regularUser = {
+    email: "shailen@gmail.com",
+    fullname: {
+        userfullname: {
             firstName: "shailendra",
-            lastName:"verma"
+            lastName: "verma"
         }
     }
 }
@@ -24,9 +24,9 @@ const regularUser ={
 
 // console.log(regularUser.fullname?.userfullname.firstName)
 
-const obj1 ={1:"a" ,2:"b"}
-const obj2 ={3:"a",4:"b"}
-const obj4 ={5:"a",5:"b"}
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+const obj4 = { 5: "a", 5: "b" }
 
 // const obj3  ={obj1,obj2}
 // {} use to store in new parenthisis the value of both onject 
@@ -34,18 +34,45 @@ const obj4 ={5:"a",5:"b"}
 
 
 // Object assign are store combine the value in object 1 store value in first objct that user {}  this 
-const obj3 = {...obj1 ,...obj2} // object .sread the glass examle this is usable 
+const obj3 = { ...obj1, ...obj2 } // object .sread the glass examle this is usable 
 
 
-console.log(obj3);
-console.log(tinderUser);
-// to cheack all values and keys of objects 
+// console.log(obj3);
+// console.log(tinderUser);
+// // to cheack all values and keys of objects 
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-// to cheack the property are exist in object or not 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"))
-console.log(tinderUser.hasOwnProperty("isLoggIn"))
-console.log(tinderUser.hasOwnProperty("name"))
+// // to cheack the property are exist in object or not 
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+// console.log(tinderUser.hasOwnProperty("isLoggIn"))
+// console.log(tinderUser.hasOwnProperty("name"))
+
+
+// Destructuring concept are use in props 
+
+const course = {
+    coursename: "js inn hindi",
+    price: 999,
+    courseInstructoe: "shailendra"
+}
+
+// course.courseInstructoe\
+const { courseInstructoe: instructor } = course
+
+console.log(instructor);
+
+// {
+//     "name" : "shailendra",
+//         "price" : "free",
+//             "coursename": "js in hindi "
+// }
+
+[
+    
+    {},
+    {},
+    {}
+]
