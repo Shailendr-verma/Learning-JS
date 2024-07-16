e29e4bdcb6ff724407d9566480922722a31c08f6
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-07-16 13:20:42 +0000

    commit change success

---
+++ 03_basics/01_function.js
@@ -0,0 +1,29 @@
+// function AddTwoNumbers(number1 ,number2){
+ 
+//     console.log(number1 + number2);
+// }
+
+// AddTwoNumbers(3,5);
+
+// 
+// function LoginUser(username ="sam"){    //give default value 
+ 
+//     return `${username} is took kind `
+// }
+
+// console.log(LoginUser("shailendra"));
+// console.log(LoginUser());
+
+// function calculateItem (...num1){   //  ...num for give value in array  by rest operator 
+
+//     return num1
+// }
+
+// console.log(calculateItem(10,21,32))
+
+// const myNewArray =[200,400,100,600]
+
+// function returnSecondVaue(getArray){
+//     return getArray[2]
+// }
+// console.log(returnSecondVaue());
