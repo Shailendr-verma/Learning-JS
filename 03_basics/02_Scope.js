d3d7f3688c28b976aa2756533fbeab6ddf5a76db
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-07-17 06:30:27 +0000

    commit change done

--- 03_basics/02_Scope.js
+++ 03_basics/02_Scope.js
@@ -1,9 +1,42 @@
-let a =10
-const b=20
-var c=30 // Global variable no limit of Scope 
+// let a =10
+// const b=20
+// var c=30 // Global variable no limit of Scope 
 // {} //Scope in lagauge
 
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// child variable access the parents 
+// icecreame 
+
+// inner variable access the out variable is known as Closer 
+// function one(){
+
+//     const username ="shailendra"
+
+//     function two(){
+//         const website ="youtube"
+
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// how to arrenge the function call   if declare variable when not call to anywhere 
+console.log(addone(5))
+function addone(num){
+    return num +1
+}
+
+// addTwo()
+// console.log(addTwo(3));
+
+const addTwo =function(num){
+    return num+2
+}
+addTwo()
+// before initilization error show
