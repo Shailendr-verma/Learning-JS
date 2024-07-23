e7308b2f793f897b37e2ef4d7559d872df4dc7b3
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-07-23 09:56:37 +0000

    done

---
+++ 05_iteration/Six.js
@@ -0,0 +1,33 @@
+// const coding = ["js", "css", "java", "javaSCRIPT"]
+
+// // forEach not return any value
+
+//  const value = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(value)
+
+
+// Filter give value back or give return value 
+// const myNums =[1,2,3,4,5,6,7,8,9,99]
+// if we scope inside the them { }  we must have to return the value 
+
+// let newNums = myNums.filter((num) => {
+//     return num>4
+// })
+// console.log(newNums);
+// const myNums =[1,2,3,4,5,6,7,8,9,99]
+
+// const newNums =[]
+// myNums.forEach((num)=>{
+//     if(num > 4 ){
+//        newNums.push(num)
+//     }
+// })
+// console.log(myNums);
+// without scope
+// let userbook = books.filter( (bk)=> bo.genere ==='history')
+// // with scope 
+//  userbook = books.filter((bk)=>{ return  bk.publidh >= 2000  && bk.genere==='history})
