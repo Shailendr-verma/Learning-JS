10b1ba18c054f3b30ef0f5917c42da0c08e57b3f
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-07-23 09:30:39 +0000

    Commit change done

---
+++ 05_iteration/five.js
@@ -0,0 +1,46 @@
+const coding = ["js" ,"css" ,"java","javaSCRIPT"]
+// callback function are without name 
+
+
+// coding.forEach( function (item){
+//         console.log(item);
+// });
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+    // this function also work as previus function print the item 
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item,index,arr)=>{
+// console.log(item,index,arr);
+// })
+
+const myCoding =[
+{
+    langaugeName :"javaScript",
+    langaugeFIleName :"js"
+},
+
+{
+    langaugeName :"java",
+    langaugeFIleName :"j"
+},
+
+{
+    langaugeName :"python",
+    langaugeFIleName :"pY"
+}
+
+]
+
+myCoding.forEach((item)=>{
+console.log(item.langaugeFIleName);
+})
+
+// this use to get value from the database and we cand use any value frome forEach loop
+
