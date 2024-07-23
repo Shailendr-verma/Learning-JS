c38a60f59e19ad9cbb5383193e64979ea495da74
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-07-23 08:12:19 +0000

    commit change

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,7 +1,15 @@
-let myArray = ['flash', 'batman', 'superman']
+// let myArray = ['flash', 'batman', 'superman']
 
-let arr =0
-while(arr <myArray.length){
-    console.log(`value of ${myArray[arr]}`);
-     arr=arr + 1
-}
+// let arr =0
+// while(arr <myArray.length){
+//     console.log(`value of ${myArray[arr]}`);
+//      arr=arr + 1
+// }
+
+// let score =1;
+ let score =11;
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <=10);
