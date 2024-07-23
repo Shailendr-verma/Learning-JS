6b74a1fe99e3007d23a270556fa4a0326182bfce
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-07-23 08:31:10 +0000

    done

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -3,5 +3,34 @@
 const arr =[1,2,3,4,5]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
+}
+
+
+const greeting ='hello world'
+for (const greet of greeting) {
+    // console.log(`each vlue of ${greet}`);
+    
+} 
+ 
+// Maps have unique vslue aprox same as array
+// Always store a unique value  
+
+
+const map = new Map()
+map.set('IN' ,"india")
+map.set('fr' ,"frence")
+map.set('k' ,"korea")
+map.set('IN', "india")
+map.set('bh',"bharat")
+console.log(map);
+
+
+for (const [key,value] of map) {
+    console.log(key ,`-:`,value);
+}
+
+const myObject ={
+    'game1':'NFS',
+    'game2': 'spiderman'
 }
