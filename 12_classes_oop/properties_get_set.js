dbf46ca84feec35bbd8a823d241a6ec6ce6fe2cd
Shailendra verma <131674031+Shailendr-verma@users.noreply.github.com>
2024-09-23 16:15:04 +0000

    done

--- 12_classes_oop/properties_get_set.js
+++ 12_classes_oop/properties_get_set.js
@@ -1,20 +1,20 @@
-function User(email, password){
+function User(email, password) {
     this._email = email;
     this._password = password
 
     Object.defineProperty(this, 'email', {
-        get: function(){
+        get: function () {
             return this._email.toUpperCase()
         },
-        set: function(value){
+        set: function (value) {
             this._email = value
         }
     })
     Object.defineProperty(this, 'password', {
-        get: function(){
+        get: function () {
             return this._password.toUpperCase()
         },
-        set: function(value){
+        set: function (value) {
             this._password = value
         }
     })
